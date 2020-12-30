ac32872cc2b63bbe36a09c728f94c246cd62890a
Егор Ермин <egorermin@yandex.ru>
2020-12-30 04:13:50 +0300

    добавлены тесты

--- StudentAppTests/StudentAppTests.swift
+++ StudentAppTests/StudentAppTests.swift
@@ -10,24 +10,113 @@ import XCTest
 
 class StudentAppTests: XCTestCase {
 
+    var serverManager: ServerManager?
+    var transToArr: TransitionToArray?
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        serverManager = ServerManager()
+        transToArr = TransitionToArray()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        serverManager = nil
+        transToArr = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAsyncRequestLoginWithIncorrectData() throws {
+        
+        // Given
+        let login = "123"
+        let password = "123"
+        let expectedResult = false
+        var workResult: Bool?
+        
+        let validatorExpectation = expectation(description: "Expectation in " + #function)
+        
+        // When
+        serverManager?.authorization(login: login, password: password, responseHandler: { (isSuccess, responseCode, _) in
+            
+            workResult = isSuccess
+            
+            validatorExpectation.fulfill()
+        })
+        
+        // Then
+        waitForExpectations(timeout: 2.0, handler: { error in
+            
+            if error != nil {
+                XCTFail()
+            }
+            
+            XCTAssertEqual(expectedResult, workResult)
+        })
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testAsyncRequestLoginWithCorrectData() throws {
+        
+        // Given
+        let login = "s1012"
+        let password = "npndtvmulczybuhnvikcfufahldkfbly"
+        let expectedResult = true
+        var workResult: Bool?
+        
+        let validatorExpectation = expectation(description: "Expectation in " + #function)
+        
+        // When
+        serverManager?.authorization(login: login, password: password, responseHandler: { (isSuccess, responseCode, _) in
+            
+            workResult = isSuccess
+            
+            validatorExpectation.fulfill()
+        })
+        
+        // Then
+        waitForExpectations(timeout: 2.0, handler: { error in
+            
+            if error != nil {
+                XCTFail()
+            }
+            
+            XCTAssertEqual(expectedResult, workResult)
+        })
+    }
+    
+    func testTransitionToArraySchedulesWithCorrectData() throws {
+        let expectedResult = true
+        let workResult: Bool?
+        
+        let schedule: [String: [Subject]] = [
+            "Monday": [Subject(name: "Maths", location: "123", time: "8:00")],
+            "Tuesday": [Subject(name: "Maths", location: "123", time: "8:00")],
+            "Wednesday": [Subject(name: "Draw", location: "321", time: "10:00")]
+        ]
+        
+        if transToArr?.transform(data: schedule) != nil {
+            workResult = true
+        } else {
+            workResult = false
         }
+        
+        XCTAssertEqual(expectedResult, workResult)
+    }
+    
+    func testTransitionToArraySchedulesWithIncorrectData() throws {
+        let expectedResult = false
+        let workResult: Bool?
+        
+        let schedule: [String: [Subject]] = [
+            "Monday": [Subject(name: "Maths", location: "123", time: "8:00")],
+            "Wensday": [Subject(name: "Draw", location: "321", time: "10:00")],
+            "Wensday123": [Subject(name: "Draw", location: "321", time: "10:00")]
+        ]
+        
+        if transToArr?.transform(data: schedule) != nil {
+            workResult = true
+        } else {
+            workResult = false
+        }
+        
+        XCTAssertEqual(expectedResult, workResult)
     }
 
 }
